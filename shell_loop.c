c5f8de699cfdfc85690388d648dd9b573417c815
seleshi27 <110991479+seleshi27@users.noreply.github.com>
2023-02-22 16:06:50 +0300

    Update shell_loop.c

--- shell_loop.c
+++ shell_loop.c
@@ -48,9 +48,9 @@ int hsh(info_t *info, char **av)
  * @info: the parameter & return info struct
  *
  * Return: -1 if builtin not found,
- * 	0 if builtin executed successfully,
- * 	1 if builtin found but not successful,
- * 	2 if builtin signals exit()
+ * 0 if builtin executed successfully,
+ * 1 if builtin found but not successful,
+ * 2 if builtin signals exit()
  */
 int find_builtin(info_t *info)
 {
@@ -157,4 +157,3 @@ void fork_cmd(info_t *info)
 				print_error(info, "Permission denied\n");
 		}
 	}
-}
