765bac1c80a2ac59f4a09b6b816e650e6d193f3d
Qudus Amidu <qudusamidu14@gmail.com>
2023-02-22 10:32:46 -0800

    builtin.c checked

--- builtin.c
+++ builtin.c
@@ -83,6 +83,7 @@ int _mycd(info_t *info)
  * _myhelp - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
  * constant function prototype.
+ *
  * Return: Always 0
  */
 int _myhelp(info_t *info)
